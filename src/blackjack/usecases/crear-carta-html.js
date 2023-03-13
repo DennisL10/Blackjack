c57e982b3e443ab590a4de196f4b8728a61a9872
DennisL10 <fl0368032019@unab.edu.sv>
2023-03-12 20:47:49 -0600

    first commit

---
+++ src/blackjack/usecases/crear-carta-html.js
@@ -0,0 +1,16 @@
+
+/**
+ *  Crea una carta en formato HTML
+ * @param {String} carta 
+ * @returns {HTMLImageElement}  Imagen de retorno
+ */
+export const crearCartaHTML = ( carta ) => {
+
+    if (!carta) throw new Error('carta es necesario');
+
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `../../public/assets/resources/${ carta }.png`; //3H, JD
+    imgCarta.classList.add('carta');
+
+    return imgCarta;
+}
---
+++ src/blackjack/usecases/turno-computadora.js
@@ -0,0 +1,46 @@
+import { pedirCarta, valorCarta } from "./";
+
+/**
+ * 
+ * @param {Number} puntosMinimos Puntos minimos que la computadora debe tener para ganar
+ * @param {HTMLElement} divCartasComputadora Elemento HTML donde se muestran las cartas de la computadora
+ * @param {HTMLElement} puntosHTML Elementos HTML que muestran los puntos
+ * @param {Array<String>} deck Deck de cartas
+ */
+export const turnoComputadora = ( puntosMinimos, puntosHTML, divCartasComputadora, deck = [] ) => {
+
+    if (!puntosMinimos) throw new Error('puntosMinimos es necesario');
+    
+
+    let puntosComputadora = 0;
+
+    do {
+        const carta = pedirCarta(deck);
+
+        puntosComputadora = puntosComputadora + valorCarta( carta );
+        puntosHTML.innerText = puntosComputadora;
+        
+        // <img class="carta" src="assets/cartas/2C.png">
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `../../public/assets/resources/${ carta }.png`; //3H, JD
+        imgCarta.classList.add('carta');
+        divCartasComputadora.append( imgCarta );
+
+        if( puntosMinimos > 21 ) {
+            break;
+        }
+
+    } while(  (puntosComputadora < puntosMinimos)  && (puntosMinimos <= 21 ) );
+
+    setTimeout(() => {
+        if( puntosComputadora === puntosMinimos ) {
+            alert('Nadie gana :(');
+        } else if ( puntosMinimos > 21 ) {
+            alert('Computadora gana')
+        } else if( puntosComputadora > 21 ) {
+            alert('Jugador Gana');
+        } else {
+            alert('Computadora Gana')
+        }
+    }, 100 );
+}
